a69390f3c644eb69fbb95dd815eb204e341384cb
jonasnico <jonas.nicolaysen@nav.no>
2024-05-02 12:36:53 +0200

    stripper ned til kun scraping. skal teste en annen DB

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
-import os
-from db import db
 from flask import Flask, jsonify, render_template
 from scraper import get_data
-from models.listing import Listing
-from routes.listings import listings
-from dotenv import load_dotenv
-from google.cloud import secretmanager
-
-load_dotenv()
 
 app = Flask(__name__)
-
-def access_secret_version(project_id, secret_id, version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-    try:
-        response = client.access_secret_version(request={"name": name})
-        return response.payload.data.decode('UTF-8')
-    except Exception as e:
-        print(f"Error: {e}")
-        print(f"Could not access secret: {name} in project: {project_id}")
-        raise
-
-if os.getenv('FLASK_ENV') == 'production':
-    app.config['SQLALCHEMY_DATABASE_URI'] = access_secret_version('oslo-quiz', 'SQLALCHEMY_DATABASE_URI')
-    app.config['SECRET_KEY'] = access_secret_version('oslo-quiz', 'SECRET_KEY')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')    
-
-app.register_blueprint(listings)
-
-db.init_app(app)
-
-def update_listings():
-    listings = get_data()
-    for listing in listings:
-        new_listing = Listing(location=listing['location'], venue=listing['venue'], date=listing['date'], time=listing['time'], category=listing['category'])
-        db.session.add(new_listing)
-    db.session.commit()
 
 @app.route('/api/listings', methods=['GET'])
 def get_listings():
@@ -53,7 +16,4 @@ def liste():
     return render_template("liste.html", title="LISTE")
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        update_listings()
     app.run(debug=True)
--- routes/listings.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from models.listing import Listing
-
-listings = Blueprint('listings', __name__)
-
-@listings.route('/api/quizer', methods=['GET'])
-def get_quizer():
-    return jsonify([{'location': listing.location, 'venue': listing.venue, 'date': listing.date, 'time': listing.time, 'category': listing.category} for listing in Listing.query.all()])
